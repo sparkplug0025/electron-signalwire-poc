ee7d346ee992b03a712e83bfa9b4267f7961c3c6
Brandon Fancher <brandonfancher@gmail.com>
2022-01-28 18:43:04 -0500

    Add screen share polyfill.

--- electron-src/index.ts
+++ electron-src/index.ts
@@ -3,7 +3,14 @@ import { join } from "path";
 import { format } from "url";
 
 // Packages
-import { BrowserWindow, app, ipcMain, IpcMainEvent } from "electron";
+import {
+  BrowserWindow,
+  app,
+  ipcMain,
+  IpcMainEvent,
+  Menu,
+  desktopCapturer,
+} from "electron";
 import isDev from "electron-is-dev";
 import prepareNext from "electron-next";
 
@@ -29,6 +36,7 @@ app.on("ready", async () => {
         slashes: true,
       });
 
+  // mainWindow.webContents.openDevTools();
   mainWindow.loadURL(url);
 });
 
@@ -39,4 +47,28 @@ app.on("window-all-closed", app.quit);
 ipcMain.on("message", (event: IpcMainEvent, message: any) => {
   console.log(message);
   setTimeout(() => event.sender.send("message", "hi from electron"), 500);
+});
+
+// display a menu for the user to select videoconf screen share sources
+ipcMain.on("show-screen-share-selector", async (event: IpcMainEvent) => {
+  const inputSources = await desktopCapturer.getSources({
+    types: ["window", "screen"],
+  });
+
+  try {
+    const selectedSourceId = await new Promise<string>((resolve, reject) => {
+      const videoOptionsMenu = Menu.buildFromTemplate(
+        inputSources.map((source) => ({
+          label: source.name,
+          click: () => resolve(source.id),
+        })),
+      );
+
+      // callback gets called after menu closes and is only fast enough if no selection was made
+      videoOptionsMenu.popup({ callback: reject });
+    });
+    event.reply("show-screen-share-selector", true, selectedSourceId);
+  } catch (e) {
+    event.reply("show-screen-share-selector", false);
+  }
 });
